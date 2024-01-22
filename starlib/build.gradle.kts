692e51e7abbb6422300152c29d91b7ad782a248d
celikgokhun <celik.gokhun@gmail.com>
2024-01-22 23:25:23 +0300

    add publishing gradle.

--- starlib/build.gradle.kts
+++ starlib/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -57,4 +58,18 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.celik.starlib"
+            artifactId = "starlib"
+            version = "1.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
 }
